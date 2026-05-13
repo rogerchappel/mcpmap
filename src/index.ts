1610bc0f935a8ab69431c0b0dece71c56f3d3aea
Roger Chappel <miscanalysis@gmail.com>
2026-05-14 06:38:46 +1000

    Export public API

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { scan } from './scan.js';
+export { parseArgs, main } from './cli.js';
+export { renderScan, renderDoctor } from './render.js';
+export type { ScanResult, ServerRecord, DoctorIssue, CliOptions } from './types.js';
